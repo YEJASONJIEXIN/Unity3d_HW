979d01a0da422b57e1a297c5a42d3d9e97f32b02
Jason <605607404@qq.com>
2020-10-13 11:56:54 +0800

    commit my homework5

---
+++ Homework5/Hit UFO/Assets/Scripts/UFOFactory.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOFactory : MonoBehaviour
+{
+    public List<GameObject> used;     //存储已经放飞的UFO
+    public List<GameObject> not_used; //存储飞回来了的UFO
+    public List<UFO_action> actions;  //存储UFO动作，小型动作管理器
+    public int round = 0;             //阶段
+    public int score = 0;             //得分
+    private void Start()
+    {
+        used = new List<GameObject>();
+        not_used = new List<GameObject>();
+        actions = new List<UFO_action>();
+        for(int i = 0; i < 10; i++)
+        {
+            not_used.Add(Object.Instantiate(Resources.Load("Prefabs/UFO", typeof(GameObject)), new Vector3(0, -20, 0), Quaternion.identity, null) as GameObject);
+            actions.Add(ScriptableObject.CreateInstance<UFO_action>());
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            actions[i].player = not_used[i];
+        }
+    }
+    private void Update()
+    {
+        if (round <= 10)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                actions[i].Update();
+            }
+            if (not_used.Count == 10)
+            {
+                round += 1;
+                if (round <= 10)
+                    get_ready(round);
+            }
+        }     
+    }
+    public void hitted(GameObject g)
+    {
+        if (g.gameObject.GetComponent<MeshRenderer>().material.color == Color.red)
+            score += 3;
+        else if (g.gameObject.GetComponent<MeshRenderer>().material.color == Color.yellow)
+            score += 2;
+        else if (g.gameObject.GetComponent<MeshRenderer>().material.color == Color.blue)
+            score += 1;
+        this.used.Remove(g);
+        g.transform.position = new Vector3(0, -20, 0);
+        for(int i = 0; i < 10; i++)
+        {
+            if (actions[i].player == g)
+                actions[i].running = false;
+        }
+        this.not_used.Add(g);
+    }
+    public void not_hit(GameObject g)
+    {
+        this.used.Remove(g);
+        g.transform.position = new Vector3(0, -20, 0);
+        for (int i = 0; i < 10; i++)
+        {
+            if (actions[i].player == g)
+                actions[i].running = false;
+        }
+        this.not_used.Add(g);
+    }
+
+    public void get_ready(int round)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            used.Add(not_used[0]);
+            not_used.Remove(not_used[0]);
+            actions[i].speed = round + 3;
+            actions[i].Start();
+            actions[i].running = true;
+        }
+    }
+}

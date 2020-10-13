8da4d3f591d114a243998725202ada36f460cb42
Jason <605607404@qq.com>
2020-10-13 13:51:20 +0800

    commit my homework6

---
+++ Homework6/HIt UFO/Assets/Scripts/UFO_Kinematics_action.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO_Kinematics_action : ScriptableObject, UFO_action
+{
+    public Director _director;
+    public GameObject player;  //记录该动作所归属的对象
+    Vector3 start;             //记录UFO飞行的初始位置
+    Vector3 end;               //记录UFO飞行的结束位置
+    public int speed=3;        //记录UFO飞行的速度
+    public bool running = true;//运行态标志位
+
+    public void SetSpeed(int speed)
+    {
+        this.speed = speed;
+    }
+    public void SetRunning(bool b)
+    {
+        this.running = b;
+    }
+    public GameObject getPlayer()
+    {
+        return this.player;
+    }
+    public void setPlayer(GameObject g)
+    {
+        this.player = g;
+    }
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _director = Director.getInstance();
+        start = new Vector3(Random.Range(-20f,20f), Random.Range(-20f, 20f), 0);
+        if (start.x < 10 && start.x > -10)
+            start.x *= 4;
+        if (start.y < 10 && start.y > -10)
+            start.y *= 4;
+        end = new Vector3(-start.x, -start.y, 0);
+        player.transform.position = start;
+        setColor();
+        Rigidbody rigit = player.GetComponent<Rigidbody>();
+        if (rigit != null)
+        {
+            Destroy(rigit);
+        }
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (running)
+        {
+            player.transform.position = Vector3.MoveTowards(player.transform.position, end, speed * Time.deltaTime);
+            if (player.transform.position == end)
+            {
+                this._director.currentController._UFOfactory.not_hit(this.player);   
+            }
+        }
+    }
+
+    public void setColor()
+    {
+        int color = Random.Range(1, 4);
+        switch (color)
+        {
+            case 1:
+                player.GetComponent<MeshRenderer>().material.color = Color.red;
+                foreach (Transform child in player.transform)
+                {
+                    child.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                }
+                break;
+            case 2:
+                player.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                foreach (Transform child in player.transform)
+                {
+                    child.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                }
+                break;
+            case 3:
+                player.GetComponent<MeshRenderer>().material.color = Color.blue;
+                foreach (Transform child in player.transform)
+                {
+                    child.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
